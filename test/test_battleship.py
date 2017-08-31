838f9c80c28d5155db0e0d5c0c9013ff167ed22e
Renzo Frigato <rfrigato@stanford.edu>
2017-08-31 09:47:33 -0700

    fix tests after live refactoring

--- test/test_battleship.py
+++ test/test_battleship.py
@@ -1,17 +1,11 @@
-from mock import MagicMock
-
 import outcomes
 import battleship
 
-
 def test_battleship():
-    board_tracker = MagicMock()
-    board_tracker.sink.return_value = outcomes.SUNK
-    ship = battleship.Battleship(3, 0, 0, True, board_tracker)
+    ship = battleship.Battleship(3)
     assert ship.hit_count == 0
     for i in range(2):
         assert ship.attack() == outcomes.HIT
         assert ship.hit_count == i + 1
     assert ship.attack() == outcomes.SUNK
-    assert board_tracker.sink.called_once()
     assert ship.hit_count == 3
--- test/test_board.py
+++ test/test_board.py
@@ -1,4 +1,4 @@
-from board import BoardTracker, Board
+from board import BoardTracker, Board, make_ship_positions
 import outcomes
 import cells
 from nose.tools import assert_raises
@@ -27,23 +27,34 @@ def test_board_empty():
 
 def test_board_place_ships():
     board = Board(3, 3)
-    board.place_ship(2, 0, 0, False)
+    board.place_ship(iter([(0, 0), (0, 1)]))
     assert board.positions[0][0].state == cells.OCCUPIED
     assert board.positions[1][0].state == cells.OCCUPIED
-    board.place_ship(2, 1, 0, True)
+    board.place_ship(iter([(1, 0), (2, 0)]))
     assert board.positions[0][1].state == cells.OCCUPIED
     assert board.positions[0][2].state == cells.OCCUPIED
-    assert_raises(Exception, board.place_ship, 2, 2, 0, False)
-    assert_raises(Exception, board.place_ship, 2, 2, 2, False)
+    assert_raises(Exception, board.place_ship, iter([(2, 0), (2, 1)]))
+    assert_raises(Exception, board.place_ship, iter([(2, 2), (2, 3)]))
 
 
 def test_board_attack():
     board = Board(3, 3)
-    board.place_ship(2, 0, 0, False)
-    board.place_ship(1, 2, 2, False)
+    board.place_ship(iter([(0, 0), (0, 1)]))
+    board.place_ship(iter([(2, 2)]))
     assert board.attack(2, 2) == outcomes.SUNK
     assert board.attack(0, 0) == outcomes.HIT
     assert board.attack(1, 0) == outcomes.MISS
     assert board.attack(1, 0) == outcomes.ALREADY_TAKEN
     assert board.attack(0, 0) == outcomes.ALREADY_TAKEN
     assert board.attack(0, 1) == outcomes.WIN
+
+
+def test_make_ship_positions():
+    positions = list(make_ship_positions(0, 0, True, 'SHIP3'))
+    assert positions == [(0, 0), (1, 0), (2, 0)]
+    positions = list(make_ship_positions(0, 0, False, 'SHIP3'))
+    assert positions == [(0, 0), (0, 1), (0, 2)]
+    positions = list(make_ship_positions(0, 0, True, 'SUBMARINE'))
+    assert positions == [(0, 0), (2, 0)]
+    positions = list(make_ship_positions(0, 0, False, 'SUBMARINE'))
+    assert positions == [(0, 0), (0, 2)]
